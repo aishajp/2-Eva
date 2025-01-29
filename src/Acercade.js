2ab0d4871f142ab17accd57149afd7dfdd439cda
aishajp <aishajp09@educastur.es>
2025-01-29 12:46:22 +0100

    cliente

---
+++ src/Acercade.js
@@ -0,0 +1,8 @@
+function AcercaDe() {
+    return (
+    <>
+    <h1>Acerca de</h1>
+    <p>Hola.<br />¿Cómo vas?</p>
+    </>
+    );
+    }
---
+++ src/Saludar.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Saludar = (props) => (
+    <>
+    <button onClick={props.saludarFn}>Si me pinchas ¿no sangro?</button>
+    </>
+    );
+    
+/*const Saludar = (props) => (
+<div>
+    <h2>
+        Hola, {props.nombre}, me he enterado de que tiene {props.edad} años
+        </h2>
+        </div>
+        );
+        export default Saludar;*/
