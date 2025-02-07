b797c85a92f5069fa2bb72ab6b2ee383b51600a6
aishajp <aishajp09@educastur.es>
2025-02-07 08:37:11 +0100

    servidores

--- Servidor/Laravel/VIDEOCLUB/routes/web.php
+++ Servidor/Laravel/VIDEOCLUB/routes/web.php
@@ -3,5 +3,42 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+   // return view('welcome');
+   echo "Hola desde Laravel";
 });
+
+/*Asi se define la 2 ruta,http://localhost:8080/2ºEva/Servidor/Laravel/VIDEOCLUB/public/pagina1 */ 
+route:: get('pagina1', function () {
+    echo "Esta es la página 1";
+});
+
+/*Asi se define la 3 ruta,http://localhost:8080/2ºEva/Servidor/Laravel/VIDEOCLUB/public/pagina2/1(numero que queramos = id) */
+route:: get('pagina2/{id}', function ($id) {
+    echo "Usuario ".$id;
+}); 
+/*Asi se define la 4 ruta,http://localhost:8080/2ºEva/Servidor/Laravel/VIDEOCLUB/public/pagina3 */
+Route::get('pagina3/{name?}', function($name = null)
+{
+return $name;
+});
+// También podemos poner algún valor por defecto...
+Route::get('pagina3_1/{name?}', function($name = 'Aisha')
+{
+return $name;
+});
+Route::get('pagina4/{name}', function($name)
+{
+//
+})
+->where('name', '[A-Za-z]+');
+Route::get('user/{id}', function($id)
+{
+//
+})
+->where('id', '[0-9]+');
+// Si hay varios parámetros podemos validarlos usando un array:
+Route::get('user/{id}/{name}', function($id, $name)
+{
+//
+})
+->where(array('id' => '[0-9]+', 'name' => '[A-Za-z]+'))
