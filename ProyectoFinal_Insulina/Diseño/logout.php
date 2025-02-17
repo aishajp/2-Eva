9644b9aa52d49238733e005c364ffef799b31443
aishajp <aishajp09@educastur.es>
2025-02-17 13:33:28 +0100

    Glucosa

--- ProyectoFinal_Insulina/Diseño/logout.php
+++ ProyectoFinal_Insulina/Diseño/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header("Location: index.html"); // Redirige a la página de inicio
+header("Location: inicio.html"); // Redirige a la página de inicio
 exit();
 ?>
