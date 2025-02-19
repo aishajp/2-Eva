90341ae577e23f85914c65a6d258b8f7bb5fcbef
aishajp <aishajp09@educastur.es>
2025-02-19 12:39:39 +0100

    Cliente

---
+++ Cliente/Unidad7/Ficheros/validacion.js
@@ -0,0 +1,31 @@
+const inicializar = () => 
+    document.getElementById('enviar')
+.addEventListener('click', validar, false);
+const validaElemento = (nombre) => {
+    let elemento = document.getElementById(nombre);
+    if (!elemento.checkValidity()){
+    error(elemento);
+    return false;
+    }
+    return true;
+    };
+    const error = (elemento) => {
+        document.getElementById('mensajeError').innerHTML = elemento.validationMessage;
+        elemento.className ="error";
+        elemento.focus();
+        };
+        const borrarError = () => {
+        let formulario = document.forms[0];
+        formulario.elements.forEach((e) => e.className="");
+        };
+        const validar = (e) => {
+            if (validaElemento("nombre") && validaElemento("edad") && confirm("Pulsa enviar si quieres mandar el formulario"))
+            return true;
+            else{
+            e.preventDefault();
+            return false;
+            }
+            };
+            
+    
+    
